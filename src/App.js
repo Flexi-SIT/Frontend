e0ba3efe0d6cdfff6632ed0540fe7bdfff1fea5f
Vardhaman Jain <vardhamanjain2401@gmail.com>
2023-02-22 13:35:24 +0530

    Added router and separate Login Pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginSection from './components/LoginSection/LoginSection';
+import HomePage from './components/HomePage/homepage';
 
 function App() {
   return (
-    <div className="App">
-      <LoginSection />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/voter" element={<LoginSection />} />
+        <Route path="/company" element={<LoginSection />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
