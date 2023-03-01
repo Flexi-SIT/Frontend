c462ce2e652b55aeeaad874ea2d46432b59cfe41
Vardhaman Jain <vardhamanjain2401@gmail.com>
2023-03-01 22:10:24 +0530

    Created Ethereum link, testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import VoterLoginSection from './pages/VoterLogin/VoterLoginSection';
 import CompanyLoginSection from './pages/CompanyLogin/CompanyLoginSection';
 import HomePage from './pages/HomePage/HomePageSection';
+import VotingList from './pages/election/VotingList';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/voter" element={<VoterLoginSection />} />
         <Route path="/company" element={<CompanyLoginSection />} />
+        <Route path="/election/vote" element={<VotingList />} />
       </Routes>
     </BrowserRouter>
 
