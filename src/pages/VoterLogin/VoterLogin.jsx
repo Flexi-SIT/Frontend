a45f8c1923a97c9b3e374dc75e9667cec2ac8f5f
Vardhaman Jain <vardhamanjain2401@gmail.com>
2023-03-03 11:04:00 +0530

    Linked MongoDB Atlas using Mongoose

--- src/pages/VoterLogin/VoterLogin.jsx
+++ src/pages/VoterLogin/VoterLogin.jsx
@@ -4,21 +4,22 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Login from './Login';
 import Logout from './Logout';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script'
 import './VoterLogin.css'
 
 import { Link, Routes, Route, useNavigate } from 'react-router-dom';
-
+import Axios from 'axios'
 
 
 const clientID = "630166332593-b2k4a2l3lq0rr8d1ko70g12qdnjb5i5a.apps.googleusercontent.com"
 
-const LoginSection = () => {
-  
+
+function LoginSection() {
+
   const navigate = useNavigate();
   const handleSubmit = event => {
-    navigate('/election/vote');
+    console.log("Registered")
   }
 
   useEffect(() => {
@@ -32,33 +33,41 @@ const LoginSection = () => {
     gapi.load('client:auth2', start)
   });
 
+
+  //Axios.post("http://localhost:3002/voterdata", { email: email, password: pass });
+
   return (
-        <Row className='d-flex flex-cloumn container'>
-            <Col className='img-container justify-content-center align-items-center'>
-              <img src='./1.svg' alt='login' className='login-img' />
-            </Col>
-            <Col className='text-container d-flex justify-content-center'>
-              <Form className='mb-4' onSubmit={handleSubmit}>
-                <Form.Group className="mb-4 login-email" controlId="formBasicEmail">
-                <Form.Control type="email" placeholder='Email Address' className='email-input'/>
-                </Form.Group>
+    <Row className='d-flex flex-cloumn container'>
+      <Col className='img-container justify-content-center align-items-center'>
+        <img src='./1.svg' alt='login' className='login-img' />
+      </Col>
+      <Col className='text-container d-flex justify-content-center'>
+        <form method="POST" action="http://localhost:3001/voter">
+          <input type="email" placeholder='Email Address' name="email"></input>
+          <input type="password" placeholder='Password' name="pass" ></input>
+          <input type="submit" />
+        </form>
+        {/* <Form className='mb-4' onSubmit={handleSubmit} method="POST" action="/voterdata">
+          <Form.Group className="mb-4 login-email" controlId="formBasicEmail">
+            <Form.Control type="email" placeholder='Email Address' className='email-input' id="email" />
+          </Form.Group>
 
-                <Form.Group className="mb-4 login-password" controlId="formBasicEmail">
-                <Form.Control type="password" placeholder='Password' className='password-input'/>
-                </Form.Group>
+          <Form.Group className="mb-4 login-password" controlId="formBasicEmail">
+            <Form.Control type="password" placeholder='Password' className='password-input' id="pass" />
+          </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Remember Me"/>
-                </Form.Group>
-                <Button variant="primary" type="submit" className='submit-button'>
-                  SIGN IN
-                </Button>
-                 <hr className='hrr'/>
-                 <Login />
-              </Form>
-              {/* <Logout /> */}
-            </Col>
-        </Row>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Remember Me" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className='submit-button'>
+            SIGN IN
+          </Button>
+          <hr className='hrr' />
+          <Login />
+        </Form> */}
+        {/* <Logout /> */}
+      </Col>
+    </Row>
   )
 }
 
---
+++ src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const VoterModel = require('./models/voter')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
+
+mongoose.connect("mongodb+srv://vardh:vardh@cluster0.aggcl0r.mongodb.net/voters?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+});
+//const voter = new VoterModel({ email: "vardh@gmail", password: "2133414" })
+//VoterModel.insertMany(voter, function (error, docs) { });
+
+
+
+app.post('/voter', (req, res) => {
+
+    const email = req.body.email
+    const pass = req.body.pass;
+    const voter = new VoterModel({ email: req.body.email, password: req.body.pass })
+    // console.log(req.body)
+    // console.log(req.body.email);
+
+
+    //const voter = new VoterModel(req.body);
+    voter.save().then(() => {
+        console.log("Success");
+    }).catch((err) => {
+        console.log(err);
+    })
+});
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001")
+});
