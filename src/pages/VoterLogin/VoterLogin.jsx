8ac237df2c02a7bca45e60825c0e84819070f8f8
Abhishek Anand <anand2000ys@gmail.com>
2023-04-10 21:43:00 +0530

    form modified

--- src/pages/VoterLogin/VoterLogin.jsx
+++ src/pages/VoterLogin/VoterLogin.jsx
@@ -1,50 +1,47 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Login from './Login';
-import Logout from './Logout';
-import { useEffect, useState } from 'react';
-import { gapi } from 'gapi-script'
-import './VoterLogin.css'
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Login from "./Login";
+import Logout from "./Logout";
+import { useEffect, useState } from "react";
+import { gapi } from "gapi-script";
+import "./VoterLogin.css";
 
-import { Link, Routes, Route, useNavigate } from 'react-router-dom';
-import Axios from 'axios'
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
+import Axios from "axios";
 
-
-const clientID = "630166332593-b2k4a2l3lq0rr8d1ko70g12qdnjb5i5a.apps.googleusercontent.com"
-
+const clientID =
+  "630166332593-b2k4a2l3lq0rr8d1ko70g12qdnjb5i5a.apps.googleusercontent.com";
 
 function LoginSection() {
-
   const navigate = useNavigate();
-  const handleSubmit = event => {
-    console.log("Registered")
-  }
+  const handleSubmit = (event) => {
+    console.log("Registered");
+  };
 
   useEffect(() => {
     function start() {
       gapi.client.init({
         clientID: clientID,
-        scope: ""
-      })
-    };
+        scope: "",
+      });
+    }
 
-    gapi.load('client:auth2', start)
+    gapi.load("client:auth2", start);
   });
-
 
   //Axios.post("http://localhost:3002/voterdata", { email: email, password: pass });
 
   return (
-    <Row className='d-flex flex-cloumn container'>
-      <Col className='img-container justify-content-center align-items-center'>
-        <img src='./1.svg' alt='login' className='login-img' />
+    <Row className="d-flex flex-cloumn container">
+      <Col className="img-container justify-content-center align-items-center">
+        <img src="./1.svg" alt="login" className="login-img" />
       </Col>
-      <Col className='text-container d-flex justify-content-center'>
+      <Col className="text-container d-flex justify-content-center">
         <form method="POST" action="http://localhost:3001/voter">
-          <input type="email" placeholder='Email Address' name="email"></input>
-          <input type="password" placeholder='Password' name="pass" ></input>
+          <input type="email" placeholder="Email Address" name="email"></input>
+          <input type="password" placeholder="Password" name="pass"></input>
           <input type="submit" />
         </form>
         {/* <Form className='mb-4' onSubmit={handleSubmit} method="POST" action="/voterdata">
@@ -68,7 +65,7 @@ function LoginSection() {
         {/* <Logout /> */}
       </Col>
     </Row>
-  )
+  );
 }
 
-export default LoginSection
+export default LoginSection;
